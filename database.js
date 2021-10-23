29f62fe590aca921165a72788410b9a319eb4c73
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-23 18:14:45 -0400

    Delete database.js

--- database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : "mongodb+srv://devdaph:C@Tk!5Haj4UkSZU@cluster0.h8uhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-    'dbName': "demo"
-};

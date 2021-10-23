51041155e3abcc74320617e2e0990b21f0790b72
Daphny Barbosa <daphnyemily@gmail.com>
2021-10-23 18:13:46 -0400

    adding 21 Savage Express App

---
+++ database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://devdaph:C@Tk!5Haj4UkSZU@cluster0.h8uhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    'dbName': "demo"
+};
